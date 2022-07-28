605794d6a9aa0ed6b3cb1707358a041de99f05a4
Melih Ulusoy <mulusoy583@gmail.com>
2022-07-28 15:43:03 +0300

    Ceaser cipher algorithm is created

---
+++ MongoDb-python/ceaserCipher.py
@@ -0,0 +1,11 @@
+# Review:
+  
+# Create a function called greet().
+def great():
+
+# Write 3 print statements inside the function.
+    print("Anakin")
+    print("Yoda")
+    print("Obiwan")
+# Call the greet() function and run your code.
+great()
