79b9ddc98b7a64a866fcec72966a9861ea4ff9a6
Melih Ulusoy <mulusoy583@gmail.com>
2022-09-07 12:31:00 +0300

    program is upgraded

--- PANDAS/datasets/test.py
+++ PANDAS/datasets/test.py
@@ -3,64 +3,75 @@ import time
 import os
 import win32com.client
 import datetime
+import pymongo
+
+
 ########        ilk dinamik değişen excel uygulaması - Pandas 
 
 
-
-
 # win32com kullanarak Excel yazılımını açma
-File = win32com.client.Dispatch("Excel.Application") 
+#File = win32com.client.Dispatch("Excel.Application") 
 
 
 # Belirlenen Excel'i açma
-Workbook = File.Workbooks.Open("C:/Users/melih/Desktop/exchange.xlsx")
+#Workbook = File.Workbooks.open("C:/Users/melih/Desktop/exchange.xlsx")
+#Workbook2 = File.Workbooks.open("C:/Users/melih/Desktop/gold.xlsx")
 
 
 
-def dateExcel():
-    # os stat bilgileri 
-    excel_status = os.stat("C:/Users/melih/Desktop/exchange.xlsx")
-    print(excel_status)
+#### Neden json formatına ihtiyaç var?   MongoDb yalnızca JSON, CSV ve TSV dosyalarını destekler. 
 
+#### NOT: MongoDb, server kısmını ifade eder. (Mongodb server tabanlı bir veri tabanıdır.)
 
-    fileSize = (excel_status.st_size/1024)                                                 # Dosya Boyutu (Kb)
-    print(f'File Size: {fileSize} Kb')
-
-
-    create_file_date=datetime.datetime.fromtimestamp(excel_status.st_ctime)                # Dosya oluşturulma tarihi
-    print(f'File creation date: {create_file_date}')
-
-
-    access_file_date=datetime.datetime.fromtimestamp(excel_status.st_atime)                # Dosyaya en son erişilme tarihi
-    print(f'File access date: {access_file_date}')
-
-
-    modify_file_date=datetime.datetime.fromtimestamp(excel_status.st_mtime)                # Dosya değiştirlme tarihi 
-    print(f'File modification date: {modify_file_date}')
-
-
-# Tüm çalışma sayfalarını yenile
-# Workbook.RefreshAll()
-
-# Çalışma sayfasını kaydetme
-# Workbook.Save()
-
-# Excel dosyasını kapatma
-# File.Quit()
+### MongoDb
 
 
 
-while True:
+my_client = pymongo.MongoClient("mongodb+srv://melih:ciRblHYkusvVbYEB@cluster0.tokxl.mongodb.net/?retryWrites=true&w=majority") 
+# Bağlantı için  "Connect your application" işaretlendi.  
+
+print(my_client.list_database_names())
+# Uzak bilgisayardaki veritabanlarını listeler.
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# while True:
     
-    dateExcel()
-    df = dataFrame = pd.read_excel("C:/Users/melih/Desktop/exchange.xlsx")
-    print(df)
-
-    Workbook.Save()
-    time.sleep(30)
-
+#     df = pd.read_excel("C:/Users/melih/Desktop/exchange.xlsx")
+#     js = df.to_json(orient = 'columns')
+#     print(df)
+#     print(js)
+#     # df2 = pd.read_excel("C:/Users/melih/Desktop/gold.xlsx")
+#     # print(df2)
+#     print("----------------------------------------------------")
+#     # dateExcel("C:/Users/melih/Desktop/exchange.xlsx")
+#     time.sleep(45)
+#     Workbook.Save()
+#     # Workbook2.Save()
     
-
-# user_select = df["Satış"]
-# print(user_select)
-
