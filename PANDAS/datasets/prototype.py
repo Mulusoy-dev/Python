3da9912d29b52e2d7ce1fd08bf46a8ec9f4849c6
Melih Ulusoy <mulusoy583@gmail.com>
2022-09-27 16:38:06 +0300

    prototype is updated

--- PANDAS/datasets/prototype.py
+++ PANDAS/datasets/prototype.py
@@ -5,7 +5,7 @@ import os
 
 
 # Mongo client
-myclient=pymongo.MongoClient("mongodb+srv://melih:CULsCMVCUKwEIyrr@cluster0.tokxl.mongodb.net/?retryWrites=true&w=majority") # SERVER bağlantısı için 
+myclient=pymongo.MongoClient("") # SERVER bağlantısı için 
 
 mydb=myclient["train-db"]                                                                                                    # train-db isminde bir veritabanı arar, bulamazsa oluşturur
 mycollection = mydb["TTS-information"]                                                                                  # exchange-information isminde bir collection ekler                                                                                       
