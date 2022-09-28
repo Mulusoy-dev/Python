4b62a36fbe009f5e941a1c60386d483f4aeb3f09
Melih Ulusoy <mulusoy583@gmail.com>
2022-09-28 11:32:36 +0300

    prototype project is upgraded

--- PANDAS/datasets/remove.py
+++ PANDAS/datasets/remove.py
@@ -1,7 +1,7 @@
 import pymongo
 
 
-myclient=pymongo.MongoClient("mongodb+srv://melih:bacMwaeP5Z47YG36@cluster0.tokxl.mongodb.net/TTS-information?retryWrites=true&w=majority") # SERVER bağlantısı için 
+myclient=pymongo.MongoClient("") # SERVER bağlantısı için 
 
 mydb=myclient["train-db"]                                                                                                    # train-db isminde bir veritabanı arar, bulamazsa oluşturur
 mycollection = mydb["TTS-information"]
