a6447b113ffd9fe19b114803886fcb59659c956c
Melih Ulusoy <mulusoy583@gmail.com>
2022-10-01 10:30:52 +0300

    sunrise-sunset-api project is upgraded

--- PANDAS/datasets/Sunrise-Sunset-Api.py
+++ PANDAS/datasets/Sunrise-Sunset-Api.py
@@ -2,45 +2,45 @@ import requests
 from datetime import datetime
 
 
-MY_LAT = "39.933365"
-MY_LONG = "32.859741"
+MY_LAT = 39.933365
+MY_LONG = 32.859741
 
 ##############################################################################
-#response = requests.get(url="http://api.open-notify.org/iss-now.json")
-#response.raise_for_status()
 
-#raw_data = response.json()
-#print(raw_data)
+def iss_overhead():
+    response_iss = requests.get(url="http://api.open-notify.org/iss-now.json")
+    response_iss.raise_for_status()
+    raw_data = response_iss.json()
 
-#longitude = raw_data["iss_position"]["longitude"]
-#print(longitude)
-#latitude = raw_data["iss_position"]["latitude"]
-#print(latitude)
-#iss_position = (longitude, latitude)
-#print(iss_position)
+    iss_longitude = float(raw_data["iss_position"]["longitude"])
+    iss_latitude = float(raw_data["iss_position"]["latitude"])
+
+    if MY_LAT-5 <= iss_longitude <= MY_LAT+5 and MY_LONG-5 <= iss_longitude <= MY_LONG+5:
+        return True
+
 
 ###############################################################################
+def is_night():
+    parameters = {
+        "lat": MY_LAT,
+        "lng": MY_LONG,
+        "formatted": 0
+    }
 
-parameters = {
-    "lat": MY_LAT,
-    "lng": MY_LONG,
-    "formatted": 0
-}
-
-response = requests.get(url="https://api.sunrise-sunset.org/json", params=parameters)
-response.raise_for_status()
-data = response.json()
-#print(data)
-
-sunrise = data["results"]["sunrise"].split("T")[1].split(":")[0]
-sunset = data["results"]["sunset"].split("T")[1].split(":")[0]
-print(sunrise)
-print(sunset)
-
-time_now = datetime.now()
-print(time_now.hour)
+    response = requests.get(url="https://api.sunrise-sunset.org/json", params=parameters)
+    response.raise_for_status()
+    data = response.json()
 
 
+    sunrise = int(data["results"]["sunrise"].split("T")[1].split(":")[0])
+    sunset = int(data["results"]["sunset"].split("T")[1].split(":")[0])
+    time_now = datetime.now().hour
+
+    if time_now >= sunset or time_now <= sunrise:
+        return True
 
 
+if iss_overhead() and is_night():
+    # send to email notification
+    pass
 
