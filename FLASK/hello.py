1f71da04e6b52df874549f753965cd13ea7b5922
Melih Ulusoy <mulusoy583@gmail.com>
2022-12-20 11:29:02 +0300

    Flask project is updated

--- FLASK/hello.py
+++ FLASK/hello.py
@@ -6,7 +6,23 @@ app = Flask(__name__)
 def hello_world():
     return 'Hello, World!'
 
+
+@app.route("/username/<name>/1")                                            # <name> str Type
+def greet(name):
+    return f"Welcome {name}!"
+
+
+@app.route("/number/<int:number>")                                         # <int> int Type
+def show_post_id(number):
+    return f"number: {number}"
+
+
+@app.route("/bye")
+def bye():
+    return "See You Again!"
+
+
 if __name__ == "__main__":
-    app.run()                                                    # equal to 'flask run'
+    app.run(debug=True)                                                    # equal to 'flask run'
 
     
