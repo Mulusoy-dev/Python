ee3b25289993758c5c6a0cdf94fc16b729bdda14
Melih Ulusoy <mulusoy583@gmail.com>
2022-12-21 17:04:25 +0300

    name card project is added

--- MongoDb-python/test.py
+++ MongoDb-python/test.py
@@ -1,11 +1,14 @@
-# from pynput.mouse import Controller
-# mouse = Controller()
-# print(mouse.position)
+from pynput.mouse import Controller
+mouse = Controller()
+
+mouse.position
+
+
 
 # (1182, 153)
 
-import keyboard
-keyboard.press('f11')
+# import keyboard
+# keyboard.press('f11')
 
 
 
